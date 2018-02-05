a7cfbf7f48a2fa50057c42e9d2b1a67c932be81d
Matt <shrwdflrst@gmail.com>
2018-02-05 18:21:04 +0000

    UPDATE setup webpack, browsersync

--- vuebnb/resources/assets/js/app.js
+++ vuebnb/resources/assets/js/app.js
@@ -1,22 +1,44 @@
+import Vue from 'vue'
+import sample from './data';
 
-/**
- * First we will load all of this project's JavaScript dependencies which
- * includes Vue and other libraries. It is a great starting point when
- * building robust, powerful web applications using Vue and Laravel.
- */
+var app = new Vue({
+    el: '#app',
+    data: {
+        title: sample.title,
+        address: sample.address,
+        about: sample.about,
+        amenities: sample.amenities,
+        prices: sample.prices,
+        contracted: true,
+        modalOpen: false,
+        headerImageStyle: {
+          'background-image': 'url(/images/header.jpg)'
+        }
+    },
+    methods: {
+      escapeKeyListener(e) {
+        if (e.keyCode === 27 && this.modalOpen) {
+          this.modalOpen = false;
+        }
+      }
+    },
+    watch: {
+      modalOpen() {
+        var className = 'modal-open';
 
-require('./bootstrap');
-
-window.Vue = require('vue');
-
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
-
-Vue.component('example-component', require('./components/ExampleComponent.vue'));
-
-const app = new Vue({
-    el: '#app'
+        if (this.modalOpen) {
+          document.body.classList.add(className);
+        } else {
+          document.body.classList.remove(className);
+        }
+      }
+    },
+    created() {
+      document.addEventListener('keyup', this.escapeKeyListener);
+    },
+    destroyed() {
+      document.removeEventListener('keyup', this.escapeKeyListener);
+    }
 });
+
+console.log('test 111');
--- vuebnb/webpack.mix.js
+++ vuebnb/webpack.mix.js
@@ -12,4 +12,29 @@ let mix = require('laravel-mix');
  */
 
 mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css');
+   .styles(
+   [
+     'node_modules/open-sans-all/css/open-sans.css',
+     'node_modules/font-awesome/css/font-awesome.css',
+     'resources/assets/css/style.css',
+   ],
+   'public/css/style.css')
+   .copy('node_modules/open-sans-all/fonts', 'public/fonts')
+   .copy('node_modules/font-awesome/fonts', 'public/fonts')
+   .copy('resources/assets/images', 'public/images')
+   .browserSync({
+    host: '192.168.10.10',
+    proxy: process.env.APP_URL,
+    open: false,
+    files: [
+        'app/**/*.php',
+        'resources/views/**/*.php',
+        'public/js/**/*.js',
+        'public/css/**/*.css'
+    ],
+    watchOptions: {
+        usePolling: true,
+        interval: 500
+    }
+  })
+;
