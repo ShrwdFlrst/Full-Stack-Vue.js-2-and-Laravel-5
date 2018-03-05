3d082abd5deef3cffb1080dfad8df6ff3740ebe9
Matt <shrwdflrst@gmail.com>
2018-03-05 14:32:40 +0000

    ADD listing summary component WIP

--- vuebnb/resources/assets/components/HomePage.vue
+++ vuebnb/resources/assets/components/HomePage.vue
@@ -1,57 +1,33 @@
 <template>
     <div class="home-container">
-        <div v-for="(group, country) in listing_groups" class="listing-summary-group">
-            <h1>Places in {{ country }}</h1>
-            <div class="listing-summaries">
-                <listing-summary
-                        v-for="listing in group"
-                        :key="listing.id"
-                        :listing="listing"
-                ></listing-summary>
-            </div>
-        </div>
+        <listing-summary-group
+                v-for="(group, country) in listing_groups"
+                :key="country"
+                :listings="group"
+                :country="country"
+                class="listing-summary-group"
+        ></listing-summary-group>
     </div>
 </template>
 <script>
+    import routeMixin from '../js/route-mixin';
+    import ListingSummaryGroup from './ListingSummaryGroup.vue';
     import {groupByCountry} from '../js/helpers';
-    import ListingSummary from './ListingSummary.vue';
-    import axios from 'axios'
-    import routerMixin from '../js/route-mixin'
 
     export default {
-        mixins: [routerMixin],
+        mixins: [routeMixin],
         data() {
             return {
                 listing_groups: []
             };
         },
-        components: {
-            ListingSummary
-        },
         methods: {
-            assignData({ listings }) {
+            assignData({listings}) {
                 this.listing_groups = groupByCountry(listings);
             }
+        },
+        components: {
+            ListingSummaryGroup
         }
     }
 </script>
-<style>
-    .listing-summary-group {
-        padding-bottom: 20px;
-    }
-
-    .listing-summaries {
-        display: flex;
-        flex-direction: row;
-        justify-content: space-between;
-        overflow: hidden;
-    }
-
-    .listing-summaries > .listing-summary {
-        margin-right: 15px;
-    }
-
-    .listing-summaries > .listing-summary:last-child {
-        margin-right: 0;
-    }
-</style>
