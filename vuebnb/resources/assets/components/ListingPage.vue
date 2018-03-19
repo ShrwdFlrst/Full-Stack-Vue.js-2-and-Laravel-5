51ee9d09b1eba6d81a7849a864c827ace1b06c53
Matt <shrwdflrst@gmail.com>
2018-03-19 17:21:49 +0000

    UPDATE store page state in Vuex

--- vuebnb/resources/assets/components/ListingPage.vue
+++ vuebnb/resources/assets/components/ListingPage.vue
@@ -1,29 +1,29 @@
 <template>
     <div>
         <header-image
-                v-if="images[0]"
-                :image-url="images[0]"
+                v-if="listing.images[0]"
+                :image-url="listing.images[0]"
                 @header-clicked="openModal"
-                :id="id"
+                :id="listing.id"
         ></header-image>
         <div class="container">
             <div class="heading">
-                <h1>{{ title }}</h1>
-                <p>{{ address }}</p>
+                <h1>{{ listing.title }}</h1>
+                <p>{{ listing.address }}</p>
             </div>
             <hr>
             <div class="about">
                 <h3>About this listing</h3>
-                <expandable-text>{{ about }}</expandable-text>
+                <expandable-text>{{ listing.about }}</expandable-text>
             </div>
             <div class="lists">
-                <feature-list title="Amenities" :items="amenities">
+                <feature-list title="Amenities" :items="listing.amenities">
                     <template slot-scope="amenity">
                         <i class="fa fa-lg" :class="amenity.icon"></i>
                         <span>{{ amenity.title }}</span>
                     </template>
                 </feature-list>
-                <feature-list title="Prices" :items="prices">
+                <feature-list title="Prices" :items="listing.prices">
                     <template slot-scope="price">
                         {{ price.title }}: <strong>{{ price.value }}</strong>
                     </template>
@@ -32,11 +32,10 @@
         </div>
 
         <modal-window ref="imagemodal">
-            <image-carousel :images="images"></image-carousel>
+            <image-carousel :images="listing.images"></image-carousel>
         </modal-window>
     </div>
 </template>
-
 <script>
     import {populateAmenitiesAndPrices} from '../js/helpers';
 
@@ -45,21 +44,8 @@
     import FeatureList from './FeatureList.vue';
     import HeaderImage from './HeaderImage.vue';
     import ExpandableText from './ExpandableText.vue';
-    import routeMixin from '../js/route-mixin';
 
     export default {
-        data() {
-            return {
-                id: null,
-                title: null,
-                about: null,
-                address: null,
-                amenities: [],
-                prices: [],
-                images: []
-            }
-        },
-        mixins: [routeMixin],
         components: {
             ImageCarousel,
             ModalWindow,
@@ -67,17 +53,21 @@
             HeaderImage,
             ExpandableText
         },
+        computed: {
+            listing() {
+                let listing = this.$store.state.listings.find(
+                    listing => listing.id == this.$route.params.listing
+                );
+                return populateAmenitiesAndPrices(listing);
+            }
+        },
         methods: {
-            assignData({listing}) {
-                Object.assign(this.$data, populateAmenitiesAndPrices(listing));
-            },
             openModal() {
                 this.$refs.imagemodal.modalOpen = true;
             }
         }
     }
 </script>
-
 <style>
     .about {
         margin-top: 2em;
